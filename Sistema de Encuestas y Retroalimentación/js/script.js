1d3046bd71b3f782ab46d91cc1fa8d2b00685223
anibaldmc <pincaybaque12@gmail.com>
2025-06-03 16:15:07 -0500

    res

--- Sistema de Encuestas y Retroalimentación/js/script.js
+++ Sistema de Encuestas y Retroalimentación/js/script.js
@@ -218,3 +218,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// bloque de preguntas
+let contadorPreguntas = 2; 
+
+document.getElementById("agregar-pregunta").addEventListener("click", () => {
+  const contenedor = document.getElementById("contenedor-preguntas");
+
+  const bloque = document.createElement("div");
+  bloque.classList.add("bloque-pregunta");
+  bloque.style.marginTop = "20px";
+
+  bloque.innerHTML = `
+    <label>Pregunta ${contadorPreguntas}:</label>
+    <input type="text" required>
+
+    <label>Ingrese respuesta</label>
+    <input type="text" required>
+
+    <label>Tipo de respuesta:</label>
+    <select>
+      <option>Opción múltiple</option>
+      <option>Respuesta corta</option>
+      <option>Escala de satisfacción</option>
+    </select>
+  `;
+
+  contenedor.appendChild(bloque);
+  contadorPreguntas++;
+});
