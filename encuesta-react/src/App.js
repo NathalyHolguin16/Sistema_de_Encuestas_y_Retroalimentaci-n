1735c613e2e174dba8baffaa2aa6fc109ba7b37a
anibaldmc <pincaybaque12@gmail.com>
2025-07-28 23:18:36 -0500

    lok

--- encuesta-react/src/App.js
+++ encuesta-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './login';
+import Login from './Login';
 import Registro from './Registro';
 import Inicio from './Inicio';
 import CrearEncuesta from './CrearEncuesta';
